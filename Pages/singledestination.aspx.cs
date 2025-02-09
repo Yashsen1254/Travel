203b0ca918aca55fe7466352b300f5f5f0c529ec
Yashsen1254 <yashsen0402@gmail.com>
2025-02-09 23:22:10 +0530

    Single Destination Model Updated

--- Pages/singledestination.aspx.cs
+++ Pages/singledestination.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Travel.Pages
 {
@@ -11,7 +13,20 @@ namespace Travel.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.QueryString["Id"] != null)
+            {
+                int Id = int.Parse(Request.QueryString["Id"]);
+                string sql = $"SELECT * FROM Destination WHERE Id = {Id}";
+                DataTable dt = utils.service.select(sql);
+                RepeaterSingle.DataSource = dt;
+                RepeaterSingle.DataBind();
+            }
+        }
 
+        protected void Book_Order(object sender, EventArgs e)
+        {
+            int Id = int.Parse(Request.QueryString["Id"]);
+            Response.Redirect("payment.aspx?Id=" + Id);
         }
     }
 }
