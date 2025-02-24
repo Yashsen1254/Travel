4ddd8f2b8122a719490d60bfb6527669fde07570
Yashsen1254 <yashsen0402@gmail.com>
2025-02-24 10:09:41 +0530

    Login Module Created

---
+++ Admin/Pages/Login/index.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Travel.Admin.Pages.Login
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login(object sender, EventArgs e)
+        {
+            DataTable dt = utils.service.select($"SELECT Id, Email, Password FROM Admins WHERE Email = '{Email.Text}' AND Password = '{Password.Text}'");
+            if (dt.Rows.Count != 0)
+            {
+                int Adminid = Convert.ToInt32(dt.Rows[0]["Id"]);
+                Session["AdminId"] = Adminid;
+                Response.Redirect("../index.aspx");
+            }
+        }
+    }
+}
